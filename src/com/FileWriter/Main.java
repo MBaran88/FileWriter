bed84e51aac042ad7ff4dd2eda1c3c566741f2b7
MBaran88 <zdzibq@gmail.com>
2017-05-31 18:47:24 +0200

    commit #1

--- src/com/FileWriter/Main.java
+++ src/com/FileWriter/Main.java
@@ -1,13 +1,11 @@
 package com.FileWriter;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        BufferedWriter fw = null;
+        /*BufferedWriter fw = null;
         try {
             fw = new BufferedWriter(new FileWriter ("abc.txt"));
             fw.append('x');
@@ -15,6 +13,37 @@ public class Main {
             fw.close();
         }
         catch (IOException e){};
-    }
+    }*/
+        //Trzeba zrobić dwie klasy: jedna która odczytuje inty z tabeli a druga która je czyta
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
 
+        try{
+            fos = new FileOutputStream("xxx.txt");
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(fos,"UTF-8"));
+            bw.write("xxxąęść");
+            bw.close();
+
+            fis = new FileInputStream("xxx.txt");
+            BufferedReader br = new BufferedReader(new InputStreamReader(fis,"UTF-8"));
+            String line = br.readLine();
+            System.out.println(line);
+            br.close();
+        }
+        catch (FileNotFoundException e){
+            e.printStackTrace();
+        }
+        catch (UnsupportedEncodingException e){
+            e.printStackTrace();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
+
+
+
+
+
+
---
+++ src/com/FileWriter/Save.java
@@ -0,0 +1,15 @@
+package com.FileWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+/**
+ * Created by RENT on 2017-05-31.
+ */
+public class Save {
+    BufferedWriter fw = null;
+    fw = new BufferedWriter(new FileWriter("txt.abc"));
+
+
+}
+
